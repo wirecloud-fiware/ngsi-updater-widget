b53884d5dfc90a98230c4f41d085027364a70c44
Álvaro Arranz <aarranz@conwet.com>
2014-05-06 14:59:46 +0200

    Initial data

---
+++ js/main.js
@@ -0,0 +1,140 @@
+/*global MashupPlatform StyledElements NGSI*/
+
+(function () {
+ 
+    "use strict";
+
+    var layout,
+        ngsi,
+        form,
+        currentData;
+
+    var create_ngsi_connection = function create_ngsi_connection() {
+        ngsi = new NGSI.Connection(MashupPlatform.prefs.get('ngsi_server'));
+    };
+
+    var init = function init() {
+        layout = new StyledElements.BorderLayout();
+        var fields = {
+            "entity" : {
+                label: 'Entity Id',
+                type: 'select',
+                initialEntries: [{
+                    label: "-------------------",
+                    value: ""
+                }],
+                required: true
+            },
+            "attribute" : {
+                label: 'Attribute Name',
+                type: 'select',
+                initialEntries: [{
+                    label: "-------------------",
+                    value: ""
+                }],
+                required: true
+            },
+            "value" : {
+                label: 'Attribute Value',
+                type: 'text',
+                required: true
+            }
+        };
+        form = new StyledElements.Form(fields, {cancelButton: false});
+        form.addEventListener("submit", updateEntity);
+        form.fieldInterfaces.entity.inputElement.addEventListener('change', onEntityChange);
+        layout.getCenterContainer().appendChild(form);
+
+        layout.insertInto(document.body);
+        layout.repaint();
+        create_ngsi_connection();
+        doQuery();
+    };
+
+    var onEntityChange = function onEntityChange(select) {
+        var attributes = Object.keys(currentData[select.getValue()]);
+        var entries = [];
+
+        for (var i = 0; i < attributes.length; i++) {
+            if (['id', 'type', 'TimeInstant'].indexOf(attributes[i]) === -1) {
+                entries.push({value: attributes[i]});
+            }
+        }
+
+        form.fieldInterfaces.attribute.inputElement.clear();
+        form.fieldInterfaces.attribute.inputElement.addEntries(entries);
+    };
+
+    var doQuery = function doQuery() {
+        ngsi.query([{
+               isPattern: true,
+               id: '.*'
+           }],
+           null,
+           {
+              flat: true,
+              onSuccess: onQuerySuccess
+           }
+        );
+    };
+
+    var onQuerySuccess = function onQuerySuccess(data) {
+        var entries = [];
+        currentData = data;
+
+        for (var key in data) {
+            entries.push({value: key});
+        }
+
+        form.fieldInterfaces['entity'].inputElement.clear();
+        form.fieldInterfaces['entity'].inputElement.addEntries(entries);
+        onEntityChange(form.fieldInterfaces.entity.inputElement);
+    };
+
+    var updateEntity = function updateEntity(form, data) {
+        var attributes =  [{
+                'name': data.attribute,
+                'contextValue': data.value
+            }];
+
+        if ('TimeInstant' in currentData[data.entity]) {
+            attributes.push({
+                'name': 'TimeInstant',
+                'contextValue': (new Date()).toISOString()
+            });
+        }
+
+        ngsi.updateAttributes([
+                {
+                    'entity': {'id': data.entity},
+                    'attributes': attributes
+                }
+            ], {
+                onSuccess: onUpdateAttributesSuccess,
+                onFailure: function (e) {
+                    document.getElementById('update_context_update').textContent = 'Fail';
+                    fail();
+                }
+            }
+        );
+    };
+
+    var onUpdateAttributesSuccess = function onUpdateAttributesSuccess() {
+        form.enable();
+    };
+
+    MashupPlatform.prefs.registerCallback(function (new_values) {
+        if ('ngsi_server' in new_values) {
+            create_ngsi_connection.call(this);
+        }
+    }.bind(this));
+
+    MashupPlatform.widget.context.registerCallback(function (newValues) {
+        if (layout && "heightInPixels" in newValues) {
+             layout.repaint();
+        }
+    }.bind(this));
+
+    window.addEventListener("DOMContentLoaded", init, false);
+
+})();
